27208c2ea597004f09407cd0e7fae12235e323c9
trigger <triggerhappy187@live.com>
2020-04-02 22:27:42 -0700

    Update main.ts
    - Include compiletime examples

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,23 @@ import { Timer, Unit } from "w3ts";
 import { Players } from "w3ts/globals";
 import { addScriptHook, W3TS_HOOK } from "w3ts/hooks";
 
+const BUILD_DATE = compiletime(() => new Date().toUTCString());
+const TS_VERSION = compiletime(() => require("typescript").version);
+const TSTL_VERSION = compiletime(() => require("typescript-to-lua").version);
+
 function tsMain() {
+  print(`Build: ${BUILD_DATE}`);
+  print(`Typescript: v${TS_VERSION}`);
+  print(`Transpiler: v${TSTL_VERSION}`);
+  print(" ");
+  print("Welcome to TypeScript!");
+
   const unit = new Unit(Players[0], FourCC("hfoo"), 0, 0, 270);
   unit.name = "TypeScript";
 
   new Timer().start(1.00, true, () => {
     unit.color = Players[math.random(0, bj_MAX_PLAYERS)].color
   });
-
-  print("Welcome to TypeScript!");
 }
 
 addScriptHook(W3TS_HOOK.MAIN_AFTER, tsMain);
