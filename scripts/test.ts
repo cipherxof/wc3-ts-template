0a63269ca0254298d095b7681a8dac912bee16c4
Henning Berge <henning@styreportalen.no>
2020-12-07 15:18:24 +0100

    add support for running within wine, with optional WINEPREFIX

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,8 +1,8 @@
-import { execFile } from "child_process";
-import { loadJsonFile, logger, compileMap } from "./utils";
+import {exec, execFile, execSync} from "child_process";
+import {loadJsonFile, logger, compileMap, IProjectConfig} from "./utils";
 
 function main() {
-  const config = loadJsonFile("config.json");
+  const config: IProjectConfig = loadJsonFile("config.json");
   const result = compileMap(config);
 
   if (!result) {
@@ -14,12 +14,19 @@ function main() {
   const filename = `${cwd}/dist/${config.mapFolder}`;
 
   logger.info(`Launching map "${filename.replace(/\\/g, "/")}"...`);
+  if(config.winePath) {
+    const wineFilename = `"Z:${filename}"`
+    const prefix = config.winePrefix ? `WINEPREFIX=${config.winePrefix}` : ''
+    execSync(`${prefix} ${config.winePath} "${config.gameExecutable}" ${["-loadfile", wineFilename, ...config.launchArgs].join(' ')}`, { stdio: 'ignore' });
+  } else {
+    execFile(config.gameExecutable, ["-loadfile", filename, ...config.launchArgs], (err: any) => {
+      if (err && err.code === 'ENOENT') {
+        logger.error(`No such file or directory "${config.gameExecutable}". Make sure gameExecutable is configured properly in config.json.`);
+      }
+    });
+  }
 
-  execFile(config.gameExecutable, ["-loadfile", filename, ...config.launchArgs], (err: any) => {
-    if (err && err.code === 'ENOENT') {
-      logger.error(`No such file or directory "${config.gameExecutable}". Make sure gameExecutable is configured properly in config.json.`);
-    }
-  });
+
 }
 
 main();
