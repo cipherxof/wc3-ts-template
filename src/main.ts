649ff4313e4ddb5a248a9f63b69672229a5870b2
trigger <triggerhappy187@live.com>
2021-10-21 22:22:22 -0700

    Upgrade packages and more
    - tstl :1.0.1
    - mdx-m3-viewer : 5.11.3
    - lua-types: 2.11.0
    - w3ts: master
    
    also added a @object-api path alias

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { Timer, Unit } from "w3ts";
 import { Players } from "w3ts/globals";
 import { addScriptHook, W3TS_HOOK } from "w3ts/hooks";
+import { ObjectData } from "@object-api/objectdata";
+import { FetchType, OEUnit } from "@object-api/objects/unit";
 
 const BUILD_DATE = compiletime(() => new Date().toUTCString());
 const TS_VERSION = compiletime(() => require("typescript").version);
 const TSTL_VERSION = compiletime(() => require("typescript-to-lua").version);
+const PEASANT = compiletime(({objectData, log}: {objectData: ObjectData, log: (s: string) => void }) => {
+  return objectData.units.get("hpea")?.fetch();
+}) as FetchType;
 
 function tsMain() {
   print(`Build: ${BUILD_DATE}`);
@@ -12,6 +17,8 @@ function tsMain() {
   print(`Transpiler: v${TSTL_VERSION}`);
   print(" ");
   print("Welcome to TypeScript!");
+  print(" ");
+  print(`Peasant costs ${PEASANT.goldCost} gold!`);
 
   const unit = new Unit(Players[0], FourCC("hfoo"), 0, 0, 270);
   unit.name = "TypeScript";
