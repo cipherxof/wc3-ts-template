024ad52bff80ddecce72199c2322ff664d51e924
trigger <triggerhappy187@live.com>
2021-11-03 12:43:59 -0700

    update package.json and compiletime example

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Timer, Unit } from "w3ts";
+import { Item, MapPlayer, Timer, Unit } from "w3ts";
 import { Players } from "w3ts/globals";
 import { addScriptHook, W3TS_HOOK } from "w3ts/hooks";
 import { ObjectData } from "@object-api/objectdata";
@@ -7,7 +7,7 @@ import { FetchType, OEUnit } from "@object-api/objects/unit";
 const BUILD_DATE = compiletime(() => new Date().toUTCString());
 const TS_VERSION = compiletime(() => require("typescript").version);
 const TSTL_VERSION = compiletime(() => require("typescript-to-lua").version);
-const PEASANT = compiletime(({objectData, log}: {objectData: ObjectData, log: (s: string) => void }) => {
+const PEASANT = compiletime(({objectData}: {objectData: ObjectData}) => {
   return objectData.units.get("hpea")?.fetch();
 }) as FetchType;
 
@@ -22,7 +22,7 @@ function tsMain() {
 
   const unit = new Unit(Players[0], FourCC("hfoo"), 0, 0, 270);
   unit.name = "TypeScript";
-
+  
   new Timer().start(1.00, true, () => {
     unit.color = Players[math.random(0, bj_MAX_PLAYERS)].color
   });
