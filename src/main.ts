970f9e717c685b9d920e58dd12cec8a89e7fcb83
trigger <triggerhappy187@live.com>
2023-05-11 23:56:10 -0700

    update packages

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,34 @@
 import { Timer, Unit } from "w3ts";
 import { Players } from "w3ts/globals";
-import { addScriptHook, W3TS_HOOK } from "w3ts/hooks";
+import { W3TS_HOOK, addScriptHook } from "w3ts/hooks";
+import { UnitContainer } from "war3-objectdata/dist/cjs/generated/units";
 
 const BUILD_DATE = compiletime(() => new Date().toUTCString());
 const TS_VERSION = compiletime(() => require("typescript").version);
 const TSTL_VERSION = compiletime(() => require("typescript-to-lua").version);
+const UNITS = compiletime((ctx: CompiletimeContext) => ctx.objectData.units) as
+  | UnitContainer
+  | undefined;
 
 function tsMain() {
-  print(`Build: ${BUILD_DATE}`);
-  print(`Typescript: v${TS_VERSION}`);
-  print(`Transpiler: v${TSTL_VERSION}`);
-  print(" ");
-  print("Welcome to TypeScript!");
+  try {
+    print(`Build: ${BUILD_DATE}`);
+    print(`Typescript: v${TS_VERSION}`);
+    print(`Transpiler: v${TSTL_VERSION}`);
+    print(" ");
+    print("Welcome to TypeScript!");
 
-  const unit = new Unit(Players[0], FourCC("hfoo"), 0, 0, 270);
-  unit.name = "TypeScript";
+    print(`Peasant's model is "' ${UNITS?.game["hpea"].modelFile}"`);
 
-  new Timer().start(1.00, true, () => {
-    unit.color = Players[math.random(0, bj_MAX_PLAYERS)].color
-  });
+    const unit = new Unit(Players[0], FourCC("hfoo"), 0, 0, 270);
+    unit.name = "TypeScript";
+
+    new Timer().start(1.0, true, () => {
+      unit.color = Players[math.random(0, bj_MAX_PLAYERS)].color;
+    });
+  } catch (e) {
+    print(e);
+  }
 }
 
 addScriptHook(W3TS_HOOK.MAIN_AFTER, tsMain);
