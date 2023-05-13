31b847f14b3b25bd1968a6554bf928192efbfb1e
trigger <triggerhappy187@live.com>
2023-05-13 00:03:31 -0700

    ready

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,11 @@
-import { Item, Timer, Unit } from "w3ts";
+import { Timer, Unit } from "w3ts";
 import { Players } from "w3ts/globals";
 import { W3TS_HOOK, addScriptHook } from "w3ts/hooks";
-import { Units } from "@objectdata/constants/units"
-
-import { Items } from "@objectdata/constants/items"
-import { UnitContainer } from "@objectdata/units";
-
-import * as CompileTimeObjects from "war3-objectdata-th"
+import { Units } from "@objectdata/units";
 
 const BUILD_DATE = compiletime(() => new Date().toUTCString());
 const TS_VERSION = compiletime(() => require("typescript").version);
 const TSTL_VERSION = compiletime(() => require("typescript-to-lua").version);
-
-compiletime((ctx: CompiletimeContext) => {
-  const items = ctx.constants.items as typeof CompileTimeObjects.Items;
-  const abilities = ctx.constants.items as typeof CompileTimeObjects.Abilities;
-
-  for (let i = 0; i < 9; i++) {
-    const item = ctx.objectData.items.copy(items.ClawsOfAttackPlus15);
-
-    if (!item) {
-      continue;
-    }
-
-    const ability = ctx.objectData.abilities.get(abilities.ItemArmorBonusPlus10);
-
-    item.newId = `I00${i}`;
-    item.name = `Generated Item ${i}`;
-    item.abilities = ability?.oldId || "";
-  }
-
-  ctx.objectData.save();
-});
 
 function tsMain() {
   try {
@@ -41,14 +15,7 @@ function tsMain() {
     print(" ");
     print("Welcome to TypeScript!");
 
-    print(Items.AlleriasFluteOfAccuracy);
-
-    for (let i = 0; i < 9; i++) {
-      Item.create(FourCC(`I00${i}`), 48 * i, 0);
-      
-    }
-    const unit = new Unit(Players[0], FourCC("hfoo"), 0, 0, 270);
-    unit.name = "TypeScript";
+    const unit = new Unit(Players[0], FourCC(Units.Footman), 0, 0, 270);
 
     new Timer().start(1.0, true, () => {
       unit.color = Players[math.random(0, bj_MAX_PLAYERS)].color;
